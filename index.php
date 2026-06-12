eed6f893689d12886845183f646adb7575dc1ce0
Muhammadbilol Qo'chqorov <mblabx@gmail.com>
2026-06-12 14:12:38 +0500

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 // ============================================================
 
 
-            $pdo = new PDO($dsn, DB_USER, DB_PASS, [
                 PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                 PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                 PDO::ATTR_EMULATE_PREPARES   => false,
